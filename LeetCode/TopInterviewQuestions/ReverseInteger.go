806b66657477b6b96047ce055aa0da8f35bd2160
captain-blue210 <captain.blue210@gmail.com>
2020-09-14 22:52:31 +0900

    solved Reverse Integer.

---
+++ LeetCode/TopInterviewQuestions/ReverseInteger.go
@@ -0,0 +1,22 @@
+package TopInterviewQuestions
+
+import "math"
+
+func reverse(x int) int {
+	reverse := 0
+	for {
+		if x != 0 {
+			pop := x % 10
+			x /= 10
+			reverse = reverse*10 + pop
+		} else {
+			break
+		}
+	}
+
+	if reverse < math.MinInt32 || reverse > math.MaxInt32 {
+		return 0
+	}
+
+	return reverse
+}
