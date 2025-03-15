d242b1bba23d3379371b6c5e28f6fb207e164718
captain-blue210 <captain.blue210@gmail.com>
2025-03-15 20:01:52 +0900

    fix: calc min price after currentProfit calc

--- typescript/src/leetcode/2025/BestTimeToBuyAndSellStock.ts
+++ typescript/src/leetcode/2025/BestTimeToBuyAndSellStock.ts
@@ -5,8 +5,8 @@ export function maxProfit(prices: number[]): number {
   let maxProfit = 0;
 
   for (let i = 1; i < prices.length; i++) {
-    minPrice = Math.min(minPrice, prices[i]);
     const currentProfit = prices[i] - minPrice;
+    minPrice = Math.min(minPrice, prices[i]);
     maxProfit = Math.max(maxProfit, currentProfit);
   }
   return maxProfit;
