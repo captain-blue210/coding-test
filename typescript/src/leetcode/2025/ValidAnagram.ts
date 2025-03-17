42a5ac086efb75d0a08d4e02a8bb00e31ddb263c
captain-blue210 <captain.blue210@gmail.com>
2025-03-17 22:14:25 +0900

    refactor: reduce if statement

--- typescript/src/leetcode/2025/ValidAnagram.ts
+++ typescript/src/leetcode/2025/ValidAnagram.ts
@@ -4,21 +4,15 @@ export function isAnagram(s: string, t: string): boolean {
   }
 
   let charCountMap: Map<string, number> = new Map();
-
   for (const char of s) {
-    if (charCountMap.has(char)) {
-      charCountMap.set(char, charCountMap.get(char)! + 1);
-    } else {
-      charCountMap.set(char, 1);
-    }
+    charCountMap.set(char, (charCountMap.get(char) || 0) + 1);
   }
 
   for (const char of t) {
-    if (charCountMap.get(char)! > 0) {
-      charCountMap.set(char, charCountMap.get(char)! - 1);
-    } else {
+    if (!charCountMap.has(char) || charCountMap.get(char)! == 0) {
       return false;
     }
+    charCountMap.set(char, charCountMap.get(char)! - 1);
   }
   return true;
 }
