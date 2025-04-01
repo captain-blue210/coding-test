711ca73a9787ef5d9672bc1e56a44149c05cdc14
captain-blue210 <captain.blue210@gmail.com>
2025-04-01 23:25:20 +0900

    solve: re-solve linked list cycle
    - 2ポインタを使って解き直した

---
+++ typescript/src/leetcode/2025/LinkedListCycleFloydAlgorhytm.ts
@@ -0,0 +1,20 @@
+import { ListNode } from "./util/ListNode";
+
+export function hasCycle(head: ListNode | null): boolean {
+  if (head == null || head.next == null) {
+    return false;
+  }
+
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head.next;
+
+  while (slow !== fast) {
+    if (fast === null || fast.next === null) {
+      return false;
+    }
+    slow = slow!.next;
+    fast = fast.next.next;
+  }
+
+  return true;
+}
