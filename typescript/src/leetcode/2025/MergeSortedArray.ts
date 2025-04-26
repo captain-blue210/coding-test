db1e15d5117039eac9bcf50c8b313609b77ec3c5
captain-blue210 <captain.blue210@gmail.com>
2025-04-26 21:55:50 +0900

    refactor: remove debug console.log statements from merge function

--- typescript/src/leetcode/2025/MergeSortedArray.ts
+++ typescript/src/leetcode/2025/MergeSortedArray.ts
@@ -17,11 +17,8 @@ export function merge(
     // nums1に有効な数字がない場合は、比較したときに絶対に小さくなる値を入れておく
     const num1Elm = i >= 0 ? nums1[i] : Number.NEGATIVE_INFINITY;
     const num2Elm = nums2[j];
-    console.log(num1Elm);
-    console.log(num2Elm);
 
     nums1[k] = Math.max(num1Elm, num2Elm);
-    console.log(nums1);
 
     // num1に追加した側のindexを減らす
     if (num1Elm > num2Elm) {
