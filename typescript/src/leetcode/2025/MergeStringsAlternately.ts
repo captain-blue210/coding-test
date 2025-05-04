52db309b379ea36bcf3d6c423c8da424591ad574
captain-blue210 <captain.blue210@gmail.com>
2025-05-04 17:44:28 +0900

    refactor: simplify mergeAlternately function by using array for result and optimizing logic

--- typescript/src/leetcode/2025/MergeStringsAlternately.ts
+++ typescript/src/leetcode/2025/MergeStringsAlternately.ts
@@ -1,27 +1,11 @@
 export function mergeAlternately(word1: string, word2: string): string {
-  let result = "";
-  let maxIndex = 0;
-  let rest = "";
-  if (word1.length < word2.length) {
-    rest = word2.slice(word1.length);
-    maxIndex = word1.length * 2;
-  } else if (word1.length > word2.length) {
-    rest = word1.slice(word2.length);
-    maxIndex = word2.length * 2;
-  } else {
-    maxIndex = word1.length + word2.length;
+  const result: string[] = [];
+  const max = Math.max(word1.length, word2.length);
+
+  for (let i = 0; i < max; i++) {
+    if (i < word1.length) result.push(word1[i]);
+    if (i < word2.length) result.push(word2[i]);
   }
 
-  let w1Index = 0;
-  let w2Index = 0;
-  while (w1Index + w2Index < maxIndex) {
-    if ((w1Index + w2Index) % 2 === 0) {
-      result += word1[w1Index];
-      w1Index++;
-    } else {
-      result += word2[w2Index];
-      w2Index++;
-    }
-  }
-  return rest ? result + rest : result;
+  return result.join("");
 }
