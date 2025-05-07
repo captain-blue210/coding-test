ff0771719e7bcf5e66386b96fe61c197bb43a890
captain-blue210 <captain.blue210@gmail.com>
2025-05-07 22:37:46 +0900

    refactor: optimize reverseVowels function by using Set for vowels and simplifying vowel swapping logic

--- typescript/src/leetcode/2025/ReverseVowelsOfAString.ts
+++ typescript/src/leetcode/2025/ReverseVowelsOfAString.ts
@@ -1,21 +1,15 @@
 export function reverseVowels(s: string): string {
-  const vowels: string[] = ["a", "i", "u", "e", "o"];
+  const vowels = new Set(["a", "e", "i", "o", "u"]);
 
-  const vowelIndices = [];
-  // 母音のある位置を記録
-  for (let i = 0; i < s.length; i++) {
-    if (vowels.includes(s.charAt(i).toLowerCase())) {
-      vowelIndices.push(i);
-    }
-  }
-
-  // 最初と後ろから母音を入れ替える
   const chars = s.split("");
   let left = 0;
-  let right = vowelIndices.length - 1;
+  let right = s.length - 1;
   while (left < right) {
-    chars.splice(vowelIndices[left], 1, s.charAt(vowelIndices[right]));
-    chars.splice(vowelIndices[right], 1, s.charAt(vowelIndices[left]));
+    while (left < right && !vowels.has(chars[left].toLowerCase())) left++;
+    while (left < right && !vowels.has(chars[right].toLowerCase())) right--;
+
+    // 左右の母音を入れ替える
+    [chars[left], chars[right]] = [chars[right], chars[left]];
 
     left++;
     right--;
