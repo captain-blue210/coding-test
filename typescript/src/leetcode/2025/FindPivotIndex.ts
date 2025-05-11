7f9dab44ae92128c95c202171f1e453f9aae57de
captain-blue210 <captain.blue210@gmail.com>
2025-05-11 17:19:12 +0900

    refactor: optimize pivotIndex function by removing unnecessary loops

--- typescript/src/leetcode/2025/FindPivotIndex.ts
+++ typescript/src/leetcode/2025/FindPivotIndex.ts
@@ -1,23 +1,14 @@
 export function pivotIndex(nums: number[]): number {
-  for (let index = 0; index < nums.length; index++) {
-    let leftIndex = 0;
-    let rightIndex = nums.length - 1;
-    let leftSome = 0;
-    let rightSome = 0;
+  // 全体の合計を計算
+  const sum = nums.reduce((sum, current) => (sum += current), 0);
 
-    while (leftIndex < index) {
-      leftSome += nums[leftIndex];
-      leftIndex++;
+  // 合計からleftSomeとcurrentIndexの値を引いてrightSomeとする
+  let leftSome = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (leftSome === sum - leftSome - nums[i]) {
+      return i;
     }
-
-    while (rightIndex > index) {
-      rightSome += nums[rightIndex];
-      rightIndex--;
-    }
-
-    if (leftSome === rightSome) {
-      return index;
-    }
+    leftSome += nums[i];
   }
   return -1;
 }
