9efae4f8eb7b1b5db93b958a3ee25e74776879ed
captain-blue210 <captain.blue210@gmail.com>
2025-08-02 20:43:21 +0900

    feat: LeetCodeに新しい問題を追加

--- typescript/src/leetcode/2025/ValidAnagram.test.ts
+++ typescript/src/leetcode/2025/ValidAnagram.test.ts
@@ -1,30 +1,30 @@
-import { isAnagram } from "../src/leetcode/2025/ValidAnagram";
+import { isAnagram } from './ValidAnagram';
 
-describe("Valid Anagram tests", () => {
+describe('Valid Anagram tests', () => {
   it('should return true when s = "anagram", t = "nagaram" is given', () => {
-    const s: string = "anagram";
-    const t: string = "nagaram";
+    const s: string = 'anagram';
+    const t: string = 'nagaram';
 
     expect(isAnagram(s, t)).toBeTruthy();
   });
 
-  it('should return true when s = "rat", t = "car" is given', () => {
-    const s: string = "rat";
-    const t: string = "car";
+  it('should return false when s = "rat", t = "car" is given', () => {
+    const s: string = 'rat';
+    const t: string = 'car';
 
     expect(isAnagram(s, t)).toBeFalsy();
   });
 
   it('should return true when s = "a", t = "a" is given', () => {
-    const s: string = "a";
-    const t: string = "a";
+    const s: string = 'a';
+    const t: string = 'a';
 
     expect(isAnagram(s, t)).toBeTruthy();
   });
 
   it('should return true when s = "ab", t = "a" is given', () => {
-    const s: string = "ab";
-    const t: string = "a";
+    const s: string = 'ab';
+    const t: string = 'a';
 
     expect(isAnagram(s, t)).toBeFalsy();
   });
--- typescript/src/leetcode/2025/ValidAnagram.ts
+++ typescript/src/leetcode/2025/ValidAnagram.ts
@@ -3,16 +3,18 @@ export function isAnagram(s: string, t: string): boolean {
     return false;
   }
 
-  let charCountMap: Map<string, number> = new Map();
-  for (const char of s) {
-    charCountMap.set(char, (charCountMap.get(char) || 0) + 1);
+  const sCountMap: Map<string, number> = new Map();
+  const tCountMap: Map<string, number> = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    sCountMap.set(s[i], (sCountMap.get(s[i]) || 0) + 1);
+    tCountMap.set(t[i], (tCountMap.get(t[i]) || 0) + 1);
   }
 
-  for (const char of t) {
-    if (!charCountMap.has(char) || charCountMap.get(char)! == 0) {
+  for (const key of sCountMap.keys()) {
+    if (sCountMap.get(key) !== tCountMap.get(key)) {
       return false;
     }
-    charCountMap.set(char, charCountMap.get(char)! - 1);
   }
   return true;
 }
